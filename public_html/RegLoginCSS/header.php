bf7be16093b02e067d1cd87cbd9bafe3a0087474
asbrar42 <35750752+asbrar42@users.noreply.github.com>
2020-06-14 00:10:24 -0500

    Add files via upload

---
+++ public_html/RegLoginCSS/header.php
@@ -0,0 +1,16 @@
+<head>
+	<title>WEBSITE</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<nav>
+	<ul>
+		<li><a href="home.php">Home</a></li>
+		<li><a href="login.php">Login</a></li>
+		<li><a href="register.php">Registration</a></li>
+		<li><a href="logout.php">Logout</a></li>
+	</ul>
+</nav>
+<?php
+	require("config.php");
+	Session_start();
+?>
