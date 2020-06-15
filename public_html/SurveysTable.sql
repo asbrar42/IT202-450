b682e356e588bf8d0593d0435610a324d3dc5f9d
asbrar42 <35750752+asbrar42@users.noreply.github.com>
2020-06-14 21:40:20 -0500

    Add files via upload

---
+++ public_html/SurveysTable.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `geo7elygwdj932pv`.`SurveysTable` ( 
+`SurveyID` INT NOT NULL,
+`UserID` INT NOT NULL,
+`Survey Name` VARCHAR(255)
+PRIMARY KEY (`SurveyID`)
+FOREIGN KEY (`UserID`)
+)
+
