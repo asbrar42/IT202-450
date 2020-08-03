cb1442bf467edb6f105107262d85a4659187592a
asbrar42 <35750752+asbrar42@users.noreply.github.com>
2020-08-03 16:46:47 -0500

    Add files via upload

---
+++ add_question.php
@@ -0,0 +1,65 @@
+<?php
+require('header.php');
+$survey_id = $_GET['surveyID'];
+?>
+
+<h1>Add Questions to Survey</h1>
+<form method="POST">
+	<label for="question">Question
+	<input type="text" name="question"/>
+	</label>
+	<label for="answer1">Answer one
+	<input type="text" name="answer1"/>
+    <label for="answer2">Answer two
+	<input type="text" name="answer2"/>
+	</label>
+	<input type="submit" name="question_form" value="Submit Question"/>
+</form>
+
+<?php 
+if(isset($_POST["question_form"])){
+	if(isset($_POST["question"]) && isset($_POST["answer1"]) && isset($_POST["answer2"])){
+		$question_text = $_POST["question"];
+        $answer1 = $_POST["answer1"];
+		$answer2 = $_POST["answer2"];
+		if (empty($question_text) || empty($answer1) || empty($answer2)){
+			echo "Fields cannot be blank!!";
+		}
+		else{
+			try{
+				$db = new PDO("mysql:host=$dbhost;dbname=$dbdatabase", $dbuser, $dbpass);
+                $question_stmt = $db->prepare("INSERT INTO questions (question_text, survey_id) VALUES(:question_text, :survey_id)");
+				$answer_stmt = $db->prepare("INSERT INTO answers (answer_text, question_id) VALUES(:answer_text, :question_id)");
+				$question_stmt->execute(array(
+					":question_text" => $question_text,
+					":survey_id" => $survey_id
+                ));
+                $stmt = $db->prepare("SELECT id FROM questions ORDER BY id DESC LIMIT 1");
+                $stmt->execute(array("%$query%"));
+                $question_id;
+                foreach($stmt as $result ){
+                    $question_id = $result['id'];
+                }
+                $answer_stmt->execute(array(
+					":answer_text" => $answer1,
+					":question_id" => $question_id
+                ));
+                $answer_stmt->execute(array(
+					":answer_text" => $answer2,
+					":question_id" => $question_id
+				));
+				$e = $stmt->errorInfo();
+				if($e[0] != "00000"){
+					echo var_export($e, true);
+				}
+				else{
+					echo "<div>Successfully added question!</div>";
+				}
+			}				
+			catch (Exception $e){
+				echo $e->getMessage();
+        }
+		}
+	}
+}
+?>
