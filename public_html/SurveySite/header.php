0ced417384222e7e39a82e5caf76888b8763975d
asbrar42 <35750752+asbrar42@users.noreply.github.com>
2020-08-02 20:36:08 -0500

    Add files via upload

---
+++ public_html/SurveySite/header.php
@@ -0,0 +1,28 @@
+<?php
+	require("config.php");
+	Session_start();
+?>
+<head>
+	<title>Arvinder's Survey Site</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<nav>
+	<ul>
+		<li>
+			<a href="home.php">Home</a>
+		</li>	
+		<li>
+			<a href="login.php">Login</a>
+		</li>
+		<li>
+			<a href="register.php">Registration</a>
+		</li>
+		<li>
+			<a href="create3.php">Create Survey</a>
+		</li>
+		<li>
+			<a href="logout.php">Logout</a>
+		</li>
+	</ul>
+</nav>
