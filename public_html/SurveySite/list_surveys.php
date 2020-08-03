221e7df0a959f34596c21e9555676babf5b6c341
asbrar42 <35750752+asbrar42@users.noreply.github.com>
2020-08-03 17:04:03 -0500

    Add files via upload

---
+++ public_html/SurveySite/list_surveys.php
@@ -0,0 +1,38 @@
+<?php
+require('header.php');
+
+$current_user = $_SESSION['user']['id'];
+$db = new PDO("mysql:host=$dbhost;dbname=$dbdatabase", $dbuser, $dbpass);
+echo "<h1>List of Surveys</h1>";
+if (isset($current_user)) {
+    try{
+        $stmt = $db->prepare("SELECT id, title FROM survey WHERE status!='public' AND user_id=$current_user");
+        $stmt->execute(array("%$query%"));
+        foreach($stmt as $result ){
+            echo "<ul>";
+            echo "<li>".$result['title']."</li><a href='add_question.php?surveyID=".$result['id']."'>Add Question</a>";
+            echo "</ul>";
+        }
+        }				
+    catch (Exception $e){
+        echo $e->getMessage();
+        echo "Error listing surveys";
+    }    
+} else{
+    try{
+        $stmt = $db->prepare("SELECT id, title FROM survey WHERE status='public'");
+        $stmt->execute(array("%$query%"));
+        foreach($stmt as $result ){
+            echo "<ul>";
+            echo "<li>".$result['title']."</li><a href='add_question.php?surveyID=".$result['id']."'>Add Question</a><br>";
+            echo "<a href='view_survey.php?surveyID=".$result['id']."'>View Survey</a><br>";
+            echo "<a href='delete_survey.php?surveyID=".$result['id']."'>Delete Survey</a>";
+            echo "</ul>";
+        }
+        }				
+    catch (Exception $e){
+        echo $e->getMessage();
+        echo "Error listing surveys";
+    }   
+}
+?>
